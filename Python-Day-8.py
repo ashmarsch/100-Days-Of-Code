19c0f8cca3e96ed5ae7c7c917e7c4b243821032b
ashmarsch <ash.mar.sch@icloud.com>
2024-10-28 20:38:58 -0600

    Add files via upload

---
+++ Python-Day-8.py
@@ -0,0 +1,20 @@
+print("Daily Affirmation Generator")
+userName = input("What is your name? ")
+print("Hello", userName, "!")
+dayOfWeek = input("What day of the week is it? ")
+if dayOfWeek == "Monday" or dayOfWeek == "monday":
+  print("I am healthy!")
+elif dayOfWeek == "Tuesday" or dayOfWeek == "tuesday":
+  print("I am loved!")
+elif dayOfWeek == "Wednesday" or dayOfWeek == "wednesday":
+  print("I am strong!")
+elif dayOfWeek == "Thursday" or dayOfWeek == "thursday":
+  print("I am beautiful!")
+elif dayOfWeek == "Friday" or dayOfWeek == "friday":
+  print("I am free!")
+elif dayOfWeek == "Saturday" or dayOfWeek == "saturday":
+  print("I am wealthy!")
+elif dayOfWeek == "Sunday" or dayOfWeek == "sunday":
+  print("I am fortunate!")
+else:
+  print("Please re-enter the day of the week.")
