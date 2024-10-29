e4005ccb10e30307d4f0607278630cb0c96318fa
ashmarsch <ash.mar.sch@icloud.com>
2024-10-28 20:09:15 -0600

    Add files via upload

---
+++ Python-Day-6.py
@@ -0,0 +1,21 @@
+userName = input("Please enter your username: ")
+if userName == "EmmaBob":
+  emPass = input("Please enter your password: ")
+  if emPass == "Emma123":
+    print("Welcome, Emma!")
+  else:
+    print("Please re-enter password.")
+elif userName == "RisseBoo":
+  rissePass = input("Please enter your password: ")
+  if rissePass == "Risse123":
+    print("Welcome, Risse!")
+  else:
+    print("Please re-enter password.")
+elif userName == "KiahJune":
+  kiahPass = input("Please enter your password: ")
+  if kiahPass == "Kiah123":
+    print("Welcome, Kiah!")
+  else:
+    print("Please re-enter password.")
+else:
+  print("I know that's you, Laney!")
