7b93af9dc70eaf6c2055394c9a99c04287e9fab9
ashmarsch <ash.mar.sch@icloud.com>
2024-10-28 19:58:24 -0600

    Add files via upload

---
+++ Python-Day-5.py
@@ -0,0 +1,17 @@
+print("Which Grandma's Boy Character Are You?")
+print("Please answer the following questions with 'yes' or 'no'.")
+questOne = input("Do you own a lion? ")
+if questOne == 'yes':
+  print("You are Dante.")
+if questOne == 'no':
+  print("Hmmm... next question: ")
+questTwo = input("Do you live with your grandma? ")
+if questTwo == 'yes':
+  print("You're Alex.")
+if questTwo == 'no':
+  print("Hmmm... next question: ")
+questThree = input("Do you want robot legs?")
+if questThree == 'yes':
+  print("You are JP.")
+if questThree == 'no':
+  print("Hmmm... Try again later, we will add more characters.")
