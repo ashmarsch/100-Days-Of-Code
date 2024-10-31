ea71f98c78c529d2e6d77f578f37a2082d7e06cc
ashmarsch <ash.mar.sch@icloud.com>
2024-10-30 20:04:47 -0600

    Delete Python-Day-10.py

--- Python-Day-10.py
+++
@@ -1,7 +0,0 @@
-totalBill = float(input("What was the total bill? $"))
-tipPercent = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
-numPeople = int(input("How many people to split the bill? "))
-tip = totalBill * (tipPercent / 100)
-totalBill += tip
-split = totalBill / numPeople
-print(f"Each person should pay: ${round(split, 2)}")
